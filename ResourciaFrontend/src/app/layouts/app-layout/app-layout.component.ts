006458d028e82043f642689e8be0675a5c927af9
Chnapak <matej.hladecek@gmail.com>
2026-02-07 20:37:27 +0100

    Site warning created

--- ResourciaFrontend/src/app/layouts/app-layout/app-layout.component.ts
+++ ResourciaFrontend/src/app/layouts/app-layout/app-layout.component.ts
@@ -1,23 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AppTopNavComponent } from './components/app-top-nav/app-top-nav.component';
+import { SiteWarningComponent } from './components/site-warning/site-warning.component';
 
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterModule, AppTopNavComponent],
+  imports: [RouterOutlet, RouterModule, AppTopNavComponent, SiteWarningComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
 
 export class AppLayoutComponent {
-  closeWarning() {
-    var notification = document.getElementById("warning");
-    notification?.classList.add("fade-out");
-    setTimeout(() => {
-      notification?.remove();
-    }, 600);
-  } 
+
 }
 
 
