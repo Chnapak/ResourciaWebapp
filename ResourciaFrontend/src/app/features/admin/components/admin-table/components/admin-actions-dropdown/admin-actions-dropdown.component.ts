28b29ee23722b4fd76f31ad7a755505191b87ead
Chnapak <matej.hladecek@gmail.com>
2026-02-20 01:56:17 +0100

    Dropdown UI created

---
+++ ResourciaFrontend/src/app/features/admin/components/admin-table/components/admin-actions-dropdown/admin-actions-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-actions-dropdown',
+  imports: [],
+  templateUrl: './admin-actions-dropdown.component.html',
+  styleUrl: './admin-actions-dropdown.component.scss'
+})
+export class AdminActionsDropdownComponent {
+
+}
