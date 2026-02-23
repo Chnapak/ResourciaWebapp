99d680839f6a19aa91356b03db3edc26364ed4ab
Chnapak <matej.hladecek@gmail.com>
2026-02-23 03:57:04 +0100

    Admin Users Hub UI + Backend

--- ResourciaBackend/src/Resourcia.Data/obj/Debug/net9.0/Resourcia.Data.AssemblyInfo.cs
+++ ResourciaBackend/src/Resourcia.Data/obj/Debug/net9.0/Resourcia.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resourcia.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9087c4ac47b10487190216c174a9b377b0f43bc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62cac8f1bec6bda4574f0d5e50f2ec168b3f1688")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resourcia.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resourcia.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
