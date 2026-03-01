35e82234f8dfa531354031013cd3fb612d3d187e
Chnapak <matej.hladecek@gmail.com>
2026-03-01 23:49:20 +0100

    Removed unnecessary backend calls

--- ResourciaFrontend/src/app/layouts/app-layout/components/app-top-nav/app-top-nav.component.ts
+++ ResourciaFrontend/src/app/layouts/app-layout/components/app-top-nav/app-top-nav.component.ts
@@ -6,6 +6,8 @@ import { ExploreMenuComponent } from './components/explore-menu/explore-menu.com
 import { NavSearchComponent } from './components/nav-search/nav-search.component';
 import { BrandLogoComponent } from './components/brand-logo/brand-logo.component';
 import { AuthLinksComponent } from './components/auth-links/auth-links.component';
+import { JwtPayloadModel } from '../../../../shared/models/jwt-payload-model';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-top-nav',
@@ -14,7 +16,7 @@ import { AuthLinksComponent } from './components/auth-links/auth-links.component
   styleUrl: './app-top-nav.component.scss'
 })
 export class AppTopNavComponent implements OnInit {
-  user: MeInfoModel | null = null;
+  user: JwtPayloadModel | null = null;
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -28,20 +30,48 @@ export class AppTopNavComponent implements OnInit {
   }
 
   loadUser(): void {
-    this.authService.getUserInfo().subscribe({
-      next: (me) => {
-        this.user = me;
-        console.log(this.user);
-        console.log(this.user.name);
-      },
-      error: () => {
-        this.user = null; // token invalid or not logged in
+    const token = this.getToken();
+
+    if (!token) {
+      this.user = null;
+      return;
+    }
+
+    try {
+      const payload = jwtDecode<JwtPayloadModel>(token);
+
+      const now = Math.floor(Date.now() / 1000);
+      if (payload.exp && payload.exp <= now) {
+        this.user = null;
+        return;
       }
-    });
+
+      const roles =
+      payload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] ?? [];
+
+      const isAdmin = roles.includes('Admin');
+
+      console.log('JWT payload:', payload);
+      this.user = {
+        id: payload.id,
+        email: payload.email,
+        name: payload.name,
+        exp: payload.exp,
+        isAdmin,
+        "http://schemas.microsoft.com/ws/2008/06/identity/claims/role": roles
+      } satisfies JwtPayloadModel;
+    } catch (error) {
+      this.user = null;
+      console.error('Failed to decode JWT:', error);
+    }
   }
 
   logout(): void {
     this.authService.logout();
+  }
+
+  private getToken(): string | null {
+    return localStorage.getItem('accessToken'); // or wherever you store it
   }
 }
 
