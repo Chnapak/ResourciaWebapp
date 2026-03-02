66297b221b2b21284dadefec7eb7f23299107e0f
Chnapak <matej.hladecek@gmail.com>
2026-03-02 20:14:16 +0100

    Basic search endpoint

---
+++ ResourciaBackend/src/Resourcia.Api/Models/Resources/ResourceSearchQueryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Resourcia.Api.Models.Resources;
+
+public class ResourceSearchQueryModel
+{
+    public string? Q { get; set; }
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
+    public Dictionary<string, List<string>> Facets { get; set; } = new();
+}
