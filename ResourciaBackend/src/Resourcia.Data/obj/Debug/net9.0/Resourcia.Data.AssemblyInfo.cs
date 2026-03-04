03335f6e318abc6cb9a5c91afed0e85c49874e8e
Chnapak <matej.hladecek@gmail.com>
2026-03-04 21:57:55 +0100

    Drag and drop frontend to backend

--- ResourciaBackend/src/Resourcia.Data/obj/Debug/net9.0/Resourcia.Data.AssemblyInfo.cs
+++ ResourciaBackend/src/Resourcia.Data/obj/Debug/net9.0/Resourcia.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resourcia.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab80a1fd8c5fa04cc0a182a6cf1ef104e97ccab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49efe899d7e6b3fedad806ab1769b03be630bb0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resourcia.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resourcia.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
