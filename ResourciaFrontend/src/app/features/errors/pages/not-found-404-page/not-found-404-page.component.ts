8d88db39b48386ac6a49f4c0a7da5bab32a4d0cc
Chnapak <matej.hladecek@gmail.com>
2026-03-07 11:12:10 +0100

    Chip component created and tested

--- ResourciaFrontend/src/app/features/errors/pages/not-found-404-page/not-found-404-page.component.ts
+++ ResourciaFrontend/src/app/features/errors/pages/not-found-404-page/not-found-404-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TextfieldComponent } from '../../../../shared/ui/textfield/textfield.component';
 import { ButtonComponent } from '../../../../shared/ui/button/button.component';
+import { ChipComponent } from '../../../../shared/ui/chip/chip.component';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [ ButtonComponent ],
+  imports: [ ButtonComponent, ChipComponent ],
   templateUrl: './not-found-404-page.component.html',
   styleUrl: './not-found-404-page.component.scss'
 })
