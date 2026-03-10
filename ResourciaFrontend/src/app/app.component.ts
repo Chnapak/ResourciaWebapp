fe7d187f1bf8be4a8d10ed8c925e4146bea006ae
Chnapak <matej.hladecek@gmail.com>
2026-03-11 00:43:40 +0100

    feat: toaster created
    Signed-off-by: Chnapak <matej.hladecek@gmail.com>

--- ResourciaFrontend/src/app/app.component.ts
+++ ResourciaFrontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ToasterComponent } from './shared/toaster/toaster.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToasterComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
