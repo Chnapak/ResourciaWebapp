6fba17bdef5f0ab00bf90d5026f256cbc54b03c6
Chnapak <matej.hladecek@gmail.com>
2026-04-05 07:02:29 +0200

    fix: another test 3 ci/cd

---
+++ ResourciaFrontend/src/environments/environment.ts
@@ -0,0 +1,11 @@
+/**
+ * Environment configuration values.
+ */
+
+/**
+ * Constant used for e nv ir on me nt configuration or shared behavior.
+ */
+export const environment = {
+    production: true,
+    siteKey: "0x4AAAAAABk867yJiHd2ouMj"
+};
