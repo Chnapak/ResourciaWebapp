dbc99c8759acc655df074468ca34307a3cde9e99
Chnapak <matej.hladecek@gmail.com>
2026-04-05 07:02:29 +0200

    fix: another test 3 ci/cd

---
+++ ResourciaFrontend/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+/**
+ * Environment configuration values.
+ */
+
+/**
+ * Constant used for environment configuration or shared behavior.
+ */
+export const environment = {
+    production: false,
+    siteKey: "0x4AAAAAABk867yJiHd2ouMj"
+};
