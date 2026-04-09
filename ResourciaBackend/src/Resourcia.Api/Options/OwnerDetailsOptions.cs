161b728287ab445faa32f78b89017053f9533bdb
Chnapak <matej.hladecek@gmail.com>
2026-04-09 19:24:50 +0200

    fix: idk tbh

--- ResourciaBackend/src/Resourcia.Api/Options/OwnerDetailsOptions.cs
+++ ResourciaBackend/src/Resourcia.Api/Options/OwnerDetailsOptions.cs
@@ -5,4 +5,5 @@ public class OwnerDetailsOptions
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
+    public string? Handle { get; set; }
 }
